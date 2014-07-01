1d29e38d6edad168f4d30aaf68b1126d28873d47
Morton Jonuschat <yabawock@gmail.com>
2014-07-01 16:09:59 +0200

    Test that map has been generated

--- test/mincer_test.js
+++ test/mincer_test.js
@@ -31,19 +31,15 @@ exports.mince = {
   },
 
   'mince manifestmap': function(test) {
-    test.expect(5);
+    test.expect(4);
 
     var manifest = grunt.file.readJSON('tmp/manifest/manifest.json');
     test.ok(manifest.assets, 'manifest is valid');
     test.ok(manifest.files, 'manifest is valid');
     test.equal(manifest.assets['main.css'].indexOf('main-'), 0, 'generated asset name starts with original asset name');
 
-    var expected = grunt.file.read('test/expected/main.css');
-    var actual = grunt.file.read('tmp/manifest/' + manifest.assets['main.css']);
-    test.equal(expected, actual, 'should mince with less compiler');
-
-    expected = grunt.file.read('test/expected/main.css.map');
-    actual = grunt.file.read('tmp/manifest/' + manifest.assets['main.css'] + '.map');
+    var expected = grunt.file.read('test/expected/main.css.map');
+    var actual = grunt.file.read('tmp/manifest/' + manifest.assets['main.css'] + '.map');
     test.equal(expected, actual, 'should generate source map');
 
     test.done();
